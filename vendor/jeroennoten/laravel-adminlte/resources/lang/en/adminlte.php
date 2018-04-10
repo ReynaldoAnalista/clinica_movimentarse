5f1b0e3cb38c22ee22108428b95b941cf3869b1d
Reynaldo <rpsouza.personal@gmail.com>
2018-04-10 12:39:03 +0000

    inclusão de arquivos vendor

---
+++ vendor/jeroennoten/laravel-adminlte/resources/lang/en/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nome Completo',
+    'email'                       => 'Email',
+    'password'                    => 'Senha',
+    'retype_password'             => 'Repita a senha',
+    'remember_me'                 => 'Lembrar de mim',
+    'register'                    => 'Register',
+    'register_a_new_membership'   => '',
+    'i_forgot_my_password'        => '',
+    'i_already_have_a_membership' => '',
+    'sign_in'                     => 'Acessar',
+    'log_out'                     => 'Sair',
+    'toggle_navigation'           => 'Mudar Navegação',
+    'login_message'               => '',
+    'register_message'            => 'Cadastrar',
+    'password_reset_message'      => 'Resetar Senha',
+    'reset_password'              => 'Resetar Senha',
+    'send_password_reset_link'    => 'Enviar Senha Resetar Link',
+];
